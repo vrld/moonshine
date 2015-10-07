e5ce82a34bd9a88a0732233f9af619521a9a5f80
camchenry <camchenry@users.noreply.github.com>
2015-10-06 22:22:46 -0400

    Updated to match the format of other shaders

--- bloom.lua
+++ bloom.lua
@@ -1,8 +1,36 @@
+--[[
+The MIT License (MIT)
+
+Copyright (c) 2015 Matthias Richter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+]]--
+
 return {
 requires = {'canvas', 'shader'},
 description = "Bloom effect",
 
 new = function(self)
+    self._size = {love.graphics.getWidth(), love.graphics.getHeight()}
+    self._samples = 5
+    self._quality = 2.5
+
     self.canvas = love.graphics.newCanvas()
     self.shader = love.graphics.newShader[[
         extern vec2 size;
@@ -29,23 +57,27 @@ new = function(self)
         }
 
     ]]
-    self.shader:send("size", {love.graphics.getWidth(), love.graphics.getHeight()})
-    self.shader:send("samples", 5)
-    self.shader:send("quality", 2.5)
+    self.shader:send("size", self._size)
+    self.shader:send("samples", self._samples)
+    self.shader:send("quality", self._quality)
 end,
 
-draw = function(self, func)
-    self:_apply_shader_to_scene(self.shader, self.canvas, func)
+draw = function(self, func, ...)
+    self:_apply_shader_to_scene(self.shader, self.canvas, func, ...)
 end,
 
 set = function(self, key, value)
     if key == "samples" then
-        self.shader:send("samples", value)
+        assert(type(value) == "number")
+        self._samples = value
+        self.shader:send(key, value)
     elseif key == "quality" then
-        self.shader:send("quality", math.max(0, math.min(10, tonumber(value) or 0)))
+        assert(type(value) == "number")
+        self._quality = value
+        self.shader:send(key, math.max(0, math.min(10, tonumber(value) or 0)))
     elseif key == "size" then
         assert(type(value) == "table")
-        self.shader:send("size", value)
+        self.shader:send(key, value)
     else
         error("Unknown property: " .. tostring(key))
     end
