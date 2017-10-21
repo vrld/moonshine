35ec7d5bce0e5b9f428d8177bf4030c4eeab0649
Matthias Richter <vrld@vrld.org>
2017-10-22 01:49:30 +0200

    Port/improve vignette

--- vignette.lua
+++ vignette.lua
@@ -1,61 +1,60 @@
 --[[
-The MIT License (MIT)
+Public domain:
 
-Copyright (c) 2015 Matthias Richter
+Copyright (C) 2017 by Matthias Richter <vrld@vrld.org>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
 ]]--
 
-return {
-description = "Vignette overlay",
+return function(shine)
+  local shader = love.graphics.newShader[[
+    extern number vignette_radius;
+    extern number vignette_softness;
+    extern number vignette_opacity;
+    extern vec4 vignette_color;
 
-new = function(self)
-	self.canvas = love.graphics.newCanvas()
-	self.shader = love.graphics.newShader[[
-		extern number radius;
-		extern number softness;
-		extern number opacity;
-		extern number aspect;
-		vec4 effect(vec4 color, Image texture, vec2 tc, vec2 _)
-		{
-			color = Texel(texture, tc);
-			number v = smoothstep(radius, radius-softness, length((tc - vec2(0.5f)) * aspect));
-			return mix(color, color * v, opacity);
-		}
-	]]
-	self.shader:send("radius",1)
-	self.shader:send("softness",.45)
-	self.shader:send("opacity",.5)
-	self.shader:send("aspect", love.graphics.getWidth() / love.graphics.getHeight())
-end,
+    vec4 effect(vec4 color, Image tex, vec2 tc, vec2 _)
+    {
+      color = Texel(tex, tc);
 
-draw = function(self, func, ...)
-	self:_apply_shader_to_scene(self.shader, self.canvas, func, ...)
-end,
+      number aspect = love_ScreenSize.x / love_ScreenSize.y;
+      number v = 1.0 - smoothstep(vignette_radius, vignette_radius-vignette_softness,
+                                  length((tc - vec2(0.5f)) * aspect));
+      return mix(color, vignette_color, v*vignette_opacity);
+    }]]
 
-set = function(self, key, value)
-	if key == "radius" or key == "softness" or key == "opacity" then
-		self.shader:send(key, math.max(0, tonumber(value) or 0))
-	else
-		error("Unknown property: " .. tostring(key))
-	end
+  local setters = {}
+  for _,k in ipairs{"radius", "softness", "opacity"} do
+    k = "vignette_"..k
+    setters[k] = function(v) shader:send(k, math.max(0, tonumber(v) or 0)) end
+  end
+  setters.vignette_color = function(c)
+    assert(type(c) == "table" and #c == 3, "Invalid value for `vignette_color'")
+    shader:send("vignette_color", {
+      (tonumber(c[1]) or 0) / 255,
+      (tonumber(c[2]) or 0) / 255,
+      (tonumber(c[3]) or 0) / 255,
+      1
+    })
+  end
 
-	return self
+  return shine.Effect{
+    shader = shader,
+    setters = setters,
+    defaults = {
+      vignette_radius = .8,
+      vignette_softness = .5,
+      vignette_opacity = .5,
+      vignette_color = {0,0,0}
+    }
+  }
 end
-}
