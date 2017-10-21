65e11326efc418e754fbecb862e9f099bfffddf3
Matthias Richter <vrld@vrld.org>
2017-10-22 01:02:28 +0200

    Port/rewrite pixelate shader

--- pixelate.lua
+++ pixelate.lua
@@ -1,123 +1,53 @@
 --[[
-The MIT License (MIT)
+Public domain:
 
-Copyright (c) 2015 Daniel Oaks, Matthias Richter
+Copyright (C) 2017 by Matthias Richter <vrld@vrld.org>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
 ]]--
 
-local function build_shader(add_original, samples)
-	local code = {[[
-	extern float pixel_size;
+return function(shine)
+  local shader = love.graphics.newShader[[
+    extern vec2 pixel_size;
+    extern number feedback;
+    vec4 effect(vec4 color, Image tex, vec2 tc, vec2 _)
+    {
+      vec4 c = Texel(tex, tc);
 
-	vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords)
-	{
-	]]}
+      // average pixel color over 5 samples
+      vec2 scale = love_ScreenSize.xy / pixel_size;
+      tc = floor(tc * scale + vec2(.5));
+      vec4 meanc = Texel(tex, tc/scale);
+      meanc += Texel(tex, (tc+vec2( 1.0,  .0))/scale);
+      meanc += Texel(tex, (tc+vec2(-1.0,  .0))/scale);
+      meanc += Texel(tex, (tc+vec2(  .0, 1.0))/scale);
+      meanc += Texel(tex, (tc+vec2(  .0,-1.0))/scale);
 
-	if add_original then
-		code[#code+1] = [[color = Texel(texture, texture_coords);]]
-	else
-		code[#code+1] = [[color = vec4(.0);]]
-	end
+      return color * mix(.2*meanc, c, feedback);
+    }
+  ]]
 
-	code[#code+1] = [[
-		vec2 pixel_pos = floor(pixel_coords / pixel_size);
-		vec2 upper_left = (pixel_pos + vec2(.2)) * pixel_size / love_ScreenSize.xy;
-		vec2 lower_right   = (pixel_pos + vec2(.8)) * pixel_size / love_ScreenSize.xy;
-		vec2 center   = (upper_left + lower_right) / 2.;
-	]]
+  local setters = {}
+  setters.pixel_size = function(v)
+    assert(type(v) == "table" and #v == 2, "Invalid value for `pixel_size'")
+    shader:send("pixel_size", v)
+  end
+  setters.feedback = function(v)
+    shader:send("feedback", math.min(1, math.max(0, tonumber(v) or 0)))
+  end
 
-	-- sample color at different positions in the pixel
-	if samples >= 1 then -- center
-		code[#code+1] = [[color += Texel(texture, center);]]
-	end
-	if samples >= 2 then -- upper left
-		code[#code+1] = [[color += Texel(texture, upper_left);]]
-	end
-	if samples >= 3 then -- lower right
-		code[#code+1] = [[color += Texel(texture, lower_right);]]
-	end
-	if samples >= 4 then -- upper right
-		code[#code+1] = [[color += Texel(texture, vec2(lower_right.x, upper_left.y));]]
-	end
-	if samples >= 5 then -- lower left
-		code[#code+1] = [[color += Texel(texture, vec2(upper_left.x, lower_right.y));]]
-	end
-	if samples >= 6 then -- center left
-		code[#code+1] = [[color += Texel(texture, vec2(upper_left.x, center.y));]]
-	end
-	if samples >= 7 then -- center right
-		code[#code+1] = [[color += Texel(texture, vec2(lower_right.x, center.y));]]
-	end
-	if samples >= 8 then -- upper center
-		code[#code+1] = [[color += Texel(texture, vec2(center.x, upper_left.y));]]
-	end
-	if samples >= 9 then -- lower center
-		code[#code+1] = [[color += Texel(texture, vec2(center.x, lower_right.y));]]
-	end
-
-	-- average out
-	code[#code+1] = ([[
-		return color / float(%d);
-	}]]):format(math.max(1, math.min(9, samples)) + (add_original and 1 or 0))
-
-	print(table.concat(code,"\n"))
-	return love.graphics.newShader(table.concat(code))
+  return shine.Effect{
+    shader = shader,
+    setters = setters,
+    defaults = {pixel_size = {5,5}, feedback = 0}
+  }
 end
-
-return {
-description = "Pixelation",
-
-new = function(self)
-	self._pixel_size = 3
-	self._add_original = false
-	self._samples = 5
-
-	self.canvas = love.graphics.newCanvas()
-	self.shader = build_shader(self._add_original, self._samples)
-
-	self.shader:send("pixel_size", self._pixel_size)
-end,
-
-draw = function(self, func, ...)
-	self:_apply_shader_to_scene(self.shader, self.canvas, func, ...)
-end,
-
-set = function(self, key, value)
-	if key == "pixel_size" then
-		assert(type(value) == "number")
-		self._pixel_size = value
-		self.shader:send("pixel_size", value)
-	elseif key == "samples" then
-		assert(type(value) == "number")
-		self._samples = value
-		self.shader = build_shader(self._add_original, self._samples)
-		self.shader:send("pixel_size", self._pixel_size)
-	elseif key == "add_original" then
-		assert(type(value) == "boolean")
-		self._add_original = value
-		self.shader = build_shader(self._add_original, self._samples)
-		self.shader:send("pixel_size", self._pixel_size)
-	else
-		error("Unknown property: " .. tostring(key))
-	end
-
-	return self
-end
-}
