a135fa50dbf793f3e744a2b013177ce688873517
Matthias Richter <vrld@vrld.org>
2017-10-21 14:26:26 +0200

    Code style

--- crt.lua
+++ crt.lua
@@ -3,17 +3,25 @@ Public domain:
 
 Copyright (C) 2017 by Matthias Richter <vrld@vrld.org>
 
-Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
 
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
 ]]--
 
 return function()
   -- Barrel distortion adapted from Daniel Oaks (see commit cef01b67fd)
   -- Added feather to mask out outside of distorted texture
-  local magnitude = {0.06, 0.065}
+  local distortionFactor
   local shader = love.graphics.newShader[[
-    extern vec2 magnitude;
+    extern vec2 distortionFactor;
+    extern vec2 scaleFactor;
     extern number feather;
 
     vec4 effect(vec4 color, Image tex, vec2 uv, vec2 px) {
@@ -21,7 +29,8 @@ return function()
       uv = uv * 2.0 - vec2(1.0);
 
       // distort
-      uv += (uv.yx*uv.yx) * uv * magnitude;
+      uv *= scaleFactor;
+      uv += (uv.yx*uv.yx) * uv * (distortionFactor - 1.0);
       number mask = (1.0 - smoothstep(1.0-feather,1.0,abs(uv.x)))
                   * (1.0 - smoothstep(1.0-feather,1.0,abs(uv.y)));
 
@@ -34,21 +43,32 @@ return function()
 
   local setters = {}
 
-  setters.magnitude = function(v)
-    assert(type(v) == "table" and #l == 2, "Invalid value for `magnitude'")
-    magnitude = {unpack(v)}
-    shader:send("magnitude", v)
+  setters.distortionFactor = function(v)
+    assert(type(v) == "table" and #l == 2, "Invalid value for `distortionFactor'")
+    distortionFactor = {unpack(v)}
+    shader:send("distortionFactor", v)
   end
 
-  setters.x = function(v) setters.magnitude{v, magnitude[2]} end
-  setters.y = function(v) setters.magnitude{magnitude[1], v} end
+  setters.x = function(v) setters.distortionFactor{v, distortionFactor[2]} end
+  setters.y = function(v) setters.distortionFactor{distortionFactor[1], v} end
+
+  setters.scaleFactor = function(v)
+    if type(v) == "table" and #l == 2 then
+      shader:send("scaleFactor", v)
+    elseif type(v) == "number" then
+      shader:send("scaleFactor", {v,v})
+    else
+      error("Invalid value for `scaleFactor'")
+    end
+  end
 
   setters.feather = function(v) shader:send("feather", v) end
 
   local defaults = {
-    magnitude = {0.06, 0.065},
-    feather = 0.02
+    distortionFactor = {1.06, 1.065},
+    feather = 0.02,
+    scale = 1,
   }
 
-  return {shader = shader, setters = setters, defaults = defaults}
+  return shine.Effect{shader = shader, setters = setters, defaults = defaults}
 end
