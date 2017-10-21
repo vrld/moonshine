04187edd1ac654f0f29187ace88bcd14730d5dc6
Matthias Richter <vrld@vrld.org>
2017-10-21 14:28:05 +0200

    Port Gaussian Blur

--- gaussianblur.lua
+++ gaussianblur.lua
@@ -1,95 +1,48 @@
 --[[
-The MIT License (MIT)
+Public domain:
 
-Copyright (c) 2015 Matthias Richter
+Copyright (C) 2017 by Matthias Richter <vrld@vrld.org>
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ]]--
+local function resetShader(sigma)
+  local support = math.max(1, math.floor(3*sigma + .5))
+  local one_by_sigma_sq = sigma > 0 and 1 / (sigma * sigma) or 1
+  local norm = 0
 
--- unroll convolution loop
-local function build_shader(sigma)
-	local support = math.max(1, math.floor(3*sigma + .5))
-	local one_by_sigma_sq = sigma > 0 and 1 / (sigma * sigma) or 1
-	local norm = 0
+  local code = {[[
+    extern vec2 direction;
+    vec4 effect(vec4 color, Image texture, vec2 tc, vec2 _)
+    { vec4 c = vec4(0.0f);
+  ]]}
+  local blur_line = "c += vec4(%f) * Texel(texture, tc + vec2(%f) * direction);"
 
-	local code = {[[
-		extern vec2 direction;
-		vec4 effect(vec4 color, Image texture, vec2 tc, vec2 _)
-		{ vec4 c = vec4(0.0f);
-	]]}
-	local blur_line = "c += vec4(%f) * Texel(texture, tc + vec2(%f) * direction);"
+  for i = -support,support do
+    local coeff = math.exp(-.5 * i*i * one_by_sigma_sq)
+    norm = norm + coeff
+    code[#code+1] = blur_line:format(coeff, i)
+  end
 
-	for i = -support,support do
-		local coeff = math.exp(-.5 * i*i * one_by_sigma_sq)
-		norm = norm + coeff
-		code[#code+1] = blur_line:format(coeff, i)
-	end
+  code[#code+1] = ("return c * vec4(%f) * color;}"):format(norm > 0 and 1/norm or 1)
 
-	code[#code+1] = ("return c * vec4(%f) * color;}"):format(norm > 0 and 1/norm or 1)
-
-	return love.graphics.newShader(table.concat(code))
+  return love.graphics.newShader(table.concat(code))
 end
 
-return {
-description = "Fast Gaussian blur shader",
+return function(shine)
+  local shader = resetShader(1)
 
-new = function(self)
-	self.canvas_h, self.canvas_v = love.graphics.newCanvas(), love.graphics.newCanvas()
-	self.shader = build_shader(1)
-	self.shader:send("direction",{1.0,0.0})
-end,
+  local setters = {}
+  setters.sigma = function(v) shader = resetShader(math.max(0,tonumber(v) or 1)) end
 
-draw = function(self, func, ...)
-	local c = love.graphics.getCanvas()
-	local s = love.graphics.getShader()
-	local co = {love.graphics.getColor()}
+  local draw = function(buffer, shader)
+    shader:send('direction', {1 / love.graphics.getWidth(), 0})
+    shine.draw_shader(buffer, shader)
 
-	-- draw scene
-	self:_render_to_canvas(self.canvas_h, func, ...)
+    self.shader:send('direction', {0, 1 / love.graphics.getHeight()})
+    shine.draw_shader(buffer, shader)
+  end
 
-	love.graphics.setColor(co)
-	love.graphics.setShader(self.shader)
-
-	local b = love.graphics.getBlendMode()
-	love.graphics.setBlendMode('alpha', 'premultiplied')
-
-	-- first pass (horizontal blur)
-	self.shader:send('direction', {1 / love.graphics.getWidth(), 0})
-	self:_render_to_canvas(self.canvas_v,
-	                       love.graphics.draw, self.canvas_h, 0,0)
-
-	-- second pass (vertical blur)
-	self.shader:send('direction', {0, 1 / love.graphics.getHeight()})
-	love.graphics.draw(self.canvas_v, 0,0)
-
-	-- restore blendmode, shader and canvas
-	love.graphics.setBlendMode(b)
-	love.graphics.setShader(s)
-	love.graphics.setCanvas(c)
-end,
-
-set = function(self, key, value)
-	if key == "sigma" then
-		self.shader = build_shader(tonumber(value))
-	else
-		error("Unknown property: " .. tostring(key))
-	end
-	return self
+  return shine.Effect{draw = draw, setters = setters}
 end
-}
