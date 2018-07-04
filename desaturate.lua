1ecab8d69f1d54fb433bf3cd5e6515b85454d715
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-07-04 03:45:46 -0700

    no floating point suffixes for shaders (iOS fix)

--- desaturate.lua
+++ desaturate.lua
@@ -21,7 +21,7 @@ return function(moonshine)
     extern number strength;
     vec4 effect(vec4 color, Image texture, vec2 tc, vec2 _) {
       color = Texel(texture, tc);
-      number luma = dot(vec3(0.299f, 0.587f, 0.114f), color.rgb);
+      number luma = dot(vec3(0.299, 0.587, 0.114), color.rgb);
       return mix(color, tint * luma, strength);
     }]]
 
